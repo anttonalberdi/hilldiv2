dbcde28363dcf036f241df20169494ab4a365743
Antton Alberdi <anttonalberdi@gmail.com>
2023-11-11 08:40:59 +0100

    dat correction

--- R/list2dist.R
+++ R/list2dist.R
@@ -10,11 +10,11 @@
 
 #Distance matrix function
 list2dist <- function(distlist){
-    dat.name1 <- as.character(dat[, 1])
-    dat.name2 <- as.character(dat[, 2])
-    dat.value <- dat[, 3]
-    names1 <- sort(unique(as.character(dat[, 1])))
-    names2 <- sort(unique(as.character(dat[, 2])))
+    distlist.name1 <- as.character(distlist[, 1])
+    distlist.name2 <- as.character(distlist[, 2])
+    distlist.value <- distlist[, 3]
+    names1 <- sort(unique(as.character(distlist[, 1])))
+    names2 <- sort(unique(as.character(distlist[, 2])))
     total.names <- unique(c(names1, names2))
     elements <- rep(NA, length(total.names)^2)
     dim(elements) <- c(length(total.names), length(total.names))
@@ -22,10 +22,10 @@ list2dist <- function(distlist){
     colnames(elements) <- total.names
     for (i in 1:length(total.names)) {
         for (j in 1:length(total.names)) {
-            for (k in 1:length(dat.name1)) {
-                if ((total.names[i] == dat.name1[k]) & (total.names[j] ==
-                  dat.name2[k])) {
-                  elements[i, j] <- dat.value[k]
+            for (k in 1:length(distlist.name1)) {
+                if ((total.names[i] == distlist.name1[k]) & (total.names[j] ==
+                  distlist.name2[k])) {
+                  elements[i, j] <- distlist.value[k]
                 }
             }
         }
